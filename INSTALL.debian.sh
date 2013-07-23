1b0c0dc0f76ae0529cec0ff85b226c4b05f8516d
nyov <nyov@nexnode.net>
2013-07-23 22:03:22 +0000

    improved distribution installation

---
+++ INSTALL.debian.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# debian
+apt-get install perl
+
+perl -MCPAN -e 'install HTML::TagParser' # not in debian
+apt-get install libhtml-linkextractor-perl libwww-perl libwww-perl liburi-perl libcss-perl libhttp-date-perl libgetopt-long-descriptive-perl
+
+apt-get install curl libcurl3 libcurl3-dev
+
+apt-get install python
